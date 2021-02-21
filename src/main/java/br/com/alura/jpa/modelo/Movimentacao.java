fc543cd9cbf2ff1dfa8a64bcf646960d61a683a2
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-02-21 04:20:53 -0300

    util class 04.03 - Adicionando categorias a movimentação

--- src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
+++ src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
@@ -2,6 +2,7 @@ package br.com.alura.jpa.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -22,9 +23,11 @@ public class Movimentacao {
 	private TipoMovimentacao tipoMovimentacao;
 	
 	private LocalDateTime data;
-
 	private String descricao;
 	private BigDecimal valor;
+	
+	@ManyToOne
+	private List<Categoria> categorias;
 	
 	@ManyToOne
 	private Conta conta;
