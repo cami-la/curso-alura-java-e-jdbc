b46bb0884e9207dd014ff49aad3b7fb5edecc733
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-02-23 05:26:59 -0300

    util class 02.02 - Carregamento Preguiçoso

---
+++ src/main/java/br/com/alura/jpa/testes/TestaRelatorioDasMovimentacoes.java
@@ -0,0 +1,32 @@
+package br.com.alura.jpa.testes;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import br.com.alura.jpa.modelo.Conta;
+
+public class TestaRelatorioDasMovimentacoes {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager(); 
+		
+		String jpql = "SELECT c FROM Conta c";
+		TypedQuery<Conta> query = em.createQuery(jpql, Conta.class);
+		
+		List<Conta> contas = query.getResultList();
+		
+		contas.stream().forEach(c ->{
+			System.out.println("Titular: " + c.getTitular());
+			System.out.println("Número: " + c.getAgencia());
+			System.out.println("Agência: " + c.getNumero());
+			System.out.println("Movimentações: " + c.getMovimentacoes());
+		});
+		
+	}
+
+}
